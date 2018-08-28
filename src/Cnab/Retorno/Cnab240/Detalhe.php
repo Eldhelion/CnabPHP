f25d299650195e8b157e68b23080e2bb1ffdb600
Helder Silva Lopes Moraes <helder.aptor@gmail.com>
2018-08-28 14:10:13 -0300

    Commentando código desnecessário

--- src/Cnab/Retorno/Cnab240/Detalhe.php
+++ src/Cnab/Retorno/Cnab240/Detalhe.php
@@ -187,12 +187,12 @@ class Detalhe extends \Cnab\Format\Linha implements \Cnab\Retorno\IDetalhe
                 $nossoNumero
             );
         }
-
+/*
         if (in_array($this->codigo_banco, array(\Cnab\Banco::SANTANDER))) {
             // retira o dv
             $nossoNumero = substr($nossoNumero, 0, -1);
         }
-
+*/
         return $nossoNumero;
     }
 
@@ -373,7 +373,7 @@ class Detalhe extends \Cnab\Format\Linha implements \Cnab\Retorno\IDetalhe
     }
 
     /**
-     * Retorna o código de liquidação, normalmente usado para 
+     * Retorna o código de liquidação, normalmente usado para
      * saber onde o cliente efetuou o pagamento.
      *
      * @return string
@@ -385,7 +385,7 @@ class Detalhe extends \Cnab\Format\Linha implements \Cnab\Retorno\IDetalhe
     }
 
     /**
-     * Retorna a descrição do código de liquidação, normalmente usado para 
+     * Retorna a descrição do código de liquidação, normalmente usado para
      * saber onde o cliente efetuou o pagamento.
      *
      * @return string
